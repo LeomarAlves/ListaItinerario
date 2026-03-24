474d09b1d7996598d7ea4a2c74aa703d61bf0848
Leomar Alves Marcelino <leo.dev.jobs@gmail.com>
2026-03-24 20:31:23 -0300

    tests implementados

--- include/lista_itinerario.h
+++ include/lista_itinerario.h
@@ -1,4 +1,16 @@
 #ifndef LISTA_ITINERARIO_H
 #define LISTA_ITINERARIO_H
 
+struct Node {
+    int conteudo;
+    Node* proximo;
+    Node* anterior;
+};
+
+struct ListaDuplamenteEncadeada {
+    int cardinalidade;
+    Node* inicio;
+    Node* fim;    
+};
+
 #endif
